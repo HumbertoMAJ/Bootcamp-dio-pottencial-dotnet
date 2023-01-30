244b087918bc444150384027fa8d1274ac92c2b3
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2023-01-30 15:40:33 -0300

    Add files via upload

---
+++ DesafiosdeProjeto/trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,24 @@
+﻿using DesafioPOO.Models;
+
+
+
+Smartphone nokia = new Nokia("55687", "F22", "N55", 556);
+Smartphone iphone = new Nokia("55687", "F22", "N55", 556);
+
+Console.WriteLine("----------TESTES-----------");
+Console.WriteLine("Nokia");
+nokia.Ligar();
+nokia.InstalarAplicativo("xadrez");
+nokia.ReceberLigacao();
+nokia.Numero = "56564";
+Console.WriteLine(nokia.Numero);
+Console.WriteLine("---------------------------");
+
+Console.WriteLine("----------TESTES-----------");
+Console.WriteLine("Iphone");
+iphone.Ligar();
+iphone.InstalarAplicativo("xadrez");
+iphone.ReceberLigacao();
+iphone.Numero = "645844";
+Console.WriteLine(iphone.Numero);
+Console.WriteLine("---------------------------");
